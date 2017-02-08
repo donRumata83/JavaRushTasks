b7b3995b4d38880f73e381429a5bd0a071731815
Dmitriy Rokachov <rokachov@gmail.com>
2017-02-08 13:51:37 +0200

    08/02/2017

---
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0140/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.task01.task0140;
+
+import java.util.Scanner;
+
+/* 
+Выводим квадрат числа
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int a = new Scanner(System.in).nextInt();
+
+        System.out.println(a*a);
+        //напишите тут ваш код
+    }
+}
