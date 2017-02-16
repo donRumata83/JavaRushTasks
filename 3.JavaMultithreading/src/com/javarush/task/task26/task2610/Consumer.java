00501ca5476837691d7f0ec566d92b604576ae18
Dmitriy Rokachov <rokachov@gmail.com>
2017-02-16 17:17:35 +0200

    16/02/2017

---
+++ 3.JavaMultithreading/src/com/javarush/task/task26/task2610/Consumer.java
@@ -0,0 +1,27 @@
+package com.javarush.task.task26.task2610;
+
+import java.util.concurrent.BlockingQueue;
+
+/**
+ * Created by Rumata on 16.02.2017.
+ */
+public class Consumer implements Runnable {
+    private BlockingQueue queue;
+
+    public Consumer(BlockingQueue queue) {
+        this.queue = queue;
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (true) {
+                System.out.println(queue.take());
+                Thread.sleep(300);
+            }
+        } catch (InterruptedException e) {
+            System.out.println(String.format("[%s] thread was terminated", Thread.currentThread().getName()));
+        }
+
+    }
+}
