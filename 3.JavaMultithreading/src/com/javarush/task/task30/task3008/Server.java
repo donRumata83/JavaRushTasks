7f2994aa0c22fb8b7efa0a679fd754076a694ced
Dmitriy Rokachov <rokachov@gmail.com>
2017-02-25 12:35:42 +0200

    25/02/2017

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
@@ -79,6 +79,54 @@ public class Server {
                 }
             }
         }
+
+        private void serverMainLoop(Connection connection, String userName) throws IOException, ClassNotFoundException {
+
+            while (true) {
+                Message message = connection.receive();
+                if (message != null && message.getType() == MessageType.TEXT) {
+                    Message newMessage = new Message(MessageType.TEXT, userName + ": " + message.getData());
+                    sendBroadcastMessage(newMessage);
+                } else {
+                    ConsoleHelper.writeMessage("Неверный тип сообщения от " + userName);
+                }
+            }
+        }
+
+        public void run() {
+            String REMOTE_ADRESS = socket.getRemoteSocketAddress().toString();
+            ConsoleHelper.writeMessage(REMOTE_ADRESS);
+            String USER_NAME = null;
+            Connection connection = null;
+            try {
+                connection = new Connection(socket);
+
+                USER_NAME = serverHandshake(connection);
+
+                if (USER_NAME != null) {
+                    sendBroadcastMessage(new Message(MessageType.USER_ADDED, USER_NAME));
+
+                    sendListOfUsers(connection, USER_NAME);
+
+                    serverMainLoop(connection, USER_NAME);
+                }
+            } catch (IOException | ClassNotFoundException e) {
+                ConsoleHelper.writeMessage("Exception with adrres" + REMOTE_ADRESS);
+                if (connection != null) {
+                    try {
+                        connection.close();
+                    } catch (IOException e1) {}
+                    if (USER_NAME != null && connectionMap.containsKey(USER_NAME)) {
+
+                        connectionMap.remove(USER_NAME);
+                        sendBroadcastMessage(new Message(MessageType.USER_REMOVED, USER_NAME));
+                    }
+                }
+
+            }
+            ConsoleHelper.writeMessage("Connection was closed");
+
+        }
     }
 }
 
