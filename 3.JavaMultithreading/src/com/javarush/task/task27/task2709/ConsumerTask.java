2288bfabebc3f0e1e3cf952bed3ba5bb79e3c473
Dmitriy Rokachov <rokachov@gmail.com>
2017-03-07 17:21:41 +0200

    2709+2711

---
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2709/ConsumerTask.java
@@ -0,0 +1,22 @@
+package com.javarush.task.task27.task2709;
+
+public class ConsumerTask implements Runnable {
+    private TransferObject transferObject;
+    protected volatile boolean stopped;
+
+    public ConsumerTask(TransferObject transferObject) {
+        this.transferObject = transferObject;
+        new Thread(this, "ConsumerTask").start();
+    }
+
+    public void run() {
+        while (!stopped) {
+           synchronized (transferObject){
+            transferObject.get();}
+        }
+    }
+
+    public void stop() {
+        stopped = true;
+    }
+}
---
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2711/Solution.java
@@ -0,0 +1,33 @@
+package com.javarush.task.task27.task2711;
+
+import java.util.concurrent.CountDownLatch;
+
+/* 
+CountDownLatch
+*/
+public class Solution {
+
+
+
+    CountDownLatch latch = new CountDownLatch(1);
+
+    public void someMethod() throws InterruptedException {
+        latch.await();
+        retrieveValue();
+        latch.countDown();
+
+
+
+
+
+
+    }
+
+    void retrieveValue() {
+        System.out.println("Value retrieved.");
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
