c8955b19ec038e17c653a57a39cb494cc7df3287
Dmitriy Rokachov <rokachov@gmail.com>
2017-03-07 18:27:45 +0200

    2710 -

---
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2710/MailServer.java
@@ -0,0 +1,18 @@
+package com.javarush.task.task27.task2710;
+
+public class MailServer implements Runnable {
+    private Mail mail;
+
+    public MailServer(Mail mail) {
+        this.mail = mail;
+    }
+
+    @Override
+    public void run() {
+        long beforeTime = System.currentTimeMillis();
+        //сделайте что-то тут - do something here
+        String name = Thread.currentThread().getName();
+        long afterTime = System.currentTimeMillis();
+        System.out.format("%s MailServer has got: [%s] in %d ms after start", name, mail.getText(), (afterTime - beforeTime));
+    }
+}
---
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2710/Solution.java
@@ -0,0 +1,15 @@
+package com.javarush.task.task27.task2710;
+
+/* 
+Расставьте wait-notify
+*/
+public class Solution {
+    public static void main(String[] args) {
+        Mail mail = new Mail();
+        Thread server = new Thread(new MailServer(mail));
+        Thread amigo = new Thread(new Person(mail));
+
+        server.start();
+        amigo.start();
+    }
+}
