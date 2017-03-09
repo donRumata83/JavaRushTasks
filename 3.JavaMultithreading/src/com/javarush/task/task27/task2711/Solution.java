a32a857edb4258a2b5314726602cf1caec1624b0
Dmitriy Rokachov <rokachov@gmail.com>
2017-03-09 17:44:07 +0200

    2710+, 3110.2 -

--- 3.JavaMultithreading/src/com/javarush/task/task27/task2711/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2711/Solution.java
@@ -6,21 +6,22 @@ import java.util.concurrent.CountDownLatch;
 CountDownLatch
 */
 public class Solution {
-
-
+    private final Object lock = new Object();
+    private volatile boolean isWaitingForValue = true;
 
     CountDownLatch latch = new CountDownLatch(1);
 
     public void someMethod() throws InterruptedException {
-        latch.await();
-        retrieveValue();
-        latch.countDown();
+        synchronized (lock) {
+            while (isWaitingForValue) {
+                lock.wait();
+            }
 
+            retrieveValue();
 
-
-
-
-
+            isWaitingForValue = false;
+            lock.notify();
+        }
     }
 
     void retrieveValue() {
