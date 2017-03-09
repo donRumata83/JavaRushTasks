e325bfaa0d11352015fe335629accbaad44aa69a
Dmitriy Rokachov <rokachov@gmail.com>
2017-03-09 18:59:55 +0200

    3110.8 -

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/CommandExecutor.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/CommandExecutor.java
@@ -5,25 +5,22 @@ import com.javarush.task.task31.task3110.command.*;
 import java.util.HashMap;
 import java.util.Map;
 
-/**
- * Created by Rumata on 09.03.2017.
- */
 public class CommandExecutor {
-    private static final Map<Operation, Command>
-            allKnownCommandsMap = new HashMap<>();
+    private static final Map<Operation, Command> allKnownCommandsMap = new HashMap<>();
+
     static {
-        allKnownCommandsMap.put(Operation.ADD, new ZipAddCommand());
         allKnownCommandsMap.put(Operation.CREATE, new ZipCreateCommand());
-        allKnownCommandsMap.put(Operation.CONTENT, new ZipContentCommand());
-        allKnownCommandsMap.put(Operation.EXTRACT, new ZipExtractCommand());
+        allKnownCommandsMap.put(Operation.ADD, new ZipAddCommand());
         allKnownCommandsMap.put(Operation.REMOVE, new ZipRemoveCommand());
+        allKnownCommandsMap.put(Operation.EXTRACT, new ZipExtractCommand());
+        allKnownCommandsMap.put(Operation.CONTENT, new ZipContentCommand());
         allKnownCommandsMap.put(Operation.EXIT, new ExitCommand());
     }
+
     private CommandExecutor() {
     }
 
-    public static void execute(Operation operation) throws Exception
-    {
+    public static void execute(Operation operation) throws Exception {
         allKnownCommandsMap.get(operation).execute();
     }
 }
