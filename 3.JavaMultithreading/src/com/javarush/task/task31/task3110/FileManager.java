60791259e90b56b6d8fca07ed00556d29f5ed1b2
Rumata <rokachov@gmail.com>
2017-03-09 23:22:45 +0200

    3110.9 +

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/FileManager.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/FileManager.java
@@ -7,9 +7,6 @@ import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Created by Татьяна on 09.03.2017.
- */
 public class FileManager {
     private Path rootPath;
     private List<Path> fileList;
@@ -25,12 +22,19 @@ public class FileManager {
     }
 
     private void collectFileList(Path path) throws IOException {
-        if (Files.isRegularFile(path))
-            fileList.add(rootPath.relativize(path));
-        else if (Files.isDirectory(path)) {
-            try (DirectoryStream<Path> dirStream = Files.newDirectoryStream(path)) {
-                for (Path subPath : dirStream) {
-                    collectFileList(subPath);
+        // Добавляем только файлы
+        if (Files.isRegularFile(path)) {
+            Path relativePath = rootPath.relativize(path);
+            fileList.add(relativePath);
+        }
+
+        // Добавляем содержимое директории
+        if (Files.isDirectory(path)) {
+            // Рекурсивно проходимся по всему содержмому директории
+            // Чтобы не писать код по вызову close для DirectoryStream, обернем вызов newDirectoryStream в try-with-resources
+            try (DirectoryStream<Path> directoryStream = Files.newDirectoryStream(path)) {
+                for (Path file : directoryStream) {
+                    collectFileList(file);
                 }
             }
         }
