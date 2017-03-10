b22a0df47f0a1d144693398c388470f785a835c3
Dmitriy Rokachov <rokachov@gmail.com>
2017-03-10 17:08:31 +0200

    3110.14 +

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipContentCommand.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipContentCommand.java
@@ -4,18 +4,22 @@ import com.javarush.task.task31.task3110.ConsoleHelper;
 import com.javarush.task.task31.task3110.FileProperties;
 import com.javarush.task.task31.task3110.ZipFileManager;
 
-/**
- * Created by Rumata on 09.03.2017.
- */
-public class ZipContentCommand extends ZipCommand{
+import java.util.List;
+
+public class ZipContentCommand extends ZipCommand {
     @Override
     public void execute() throws Exception {
         ConsoleHelper.writeMessage("Просмотр содержимого архива.");
+
         ZipFileManager zipFileManager = getZipFileManager();
+
         ConsoleHelper.writeMessage("Содержимое архива:");
-        for (FileProperties filePrperty: zipFileManager.getFilesList()) {
-            ConsoleHelper.writeMessage(filePrperty.toString());
+
+        List<FileProperties> files = zipFileManager.getFilesList();
+        for (FileProperties file : files) {
+            ConsoleHelper.writeMessage(file.toString());
         }
+
         ConsoleHelper.writeMessage("Содержимое архива прочитано.");
     }
 }
