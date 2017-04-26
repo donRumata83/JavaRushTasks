97a33a25753535d92c01c1f5ffd9977474096268
Dmitriy Rokachov <rokachov@gmail.com>
2017-04-26 13:36:30 +0300

    3209.12+

--- 3.JavaMultithreading/src/com/javarush/task/task32/task3209/actions/RedoAction.java
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/actions/RedoAction.java
@@ -5,15 +5,13 @@ import com.javarush.task.task32.task3209.View;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 
-/**
- * Created by Rumata on 25.04.2017.
- */
+
 public class RedoAction extends AbstractAction {
     private View view;
     public RedoAction(View view) {this.view = view;}
 
     @Override
     public void actionPerformed(ActionEvent e) {
-
+        view.redo();
     }
 }
--- 3.JavaMultithreading/src/com/javarush/task/task32/task3209/actions/UndoAction.java
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/actions/UndoAction.java
@@ -15,6 +15,6 @@ public class UndoAction extends AbstractAction {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-
+        view.undo();
     }
 }
