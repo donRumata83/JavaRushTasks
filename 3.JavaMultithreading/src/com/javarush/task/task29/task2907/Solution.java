2d3b601576f74839f055aa207391ee57014efc11
Dmitriy Rokachov <rokachov@gmail.com>
2017-04-28 18:20:17 +0300

    2907, 2908 +

---
+++ 3.JavaMultithreading/src/com/javarush/task/task29/task2907/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.task29.task2907;
+
+import java.math.BigDecimal;
+
+/* 
+Этот странный BigDecimal
+*/
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(getValue(1.1d, 1.2d));
+    }
+
+    public static BigDecimal getValue(double v1, double v2) {
+        return new BigDecimal(String.valueOf(v1)).add(new BigDecimal(String.valueOf(v2)));
+    }
+}
