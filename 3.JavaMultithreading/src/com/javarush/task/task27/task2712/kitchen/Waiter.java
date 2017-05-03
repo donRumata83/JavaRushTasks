428da15389626354ab8648170c44d7d0304ca9e6
Dmitriy Rokachov <rokachov@gmail.com>
2017-05-03 18:16:30 +0300

    2712.3 +

---
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2712/kitchen/Waiter.java
@@ -0,0 +1,16 @@
+package com.javarush.task.task27.task2712.kitchen;
+
+import javafx.beans.Observable;
+
+import java.util.Observer;
+
+/**
+ * Created by Rumata on 03.05.2017.
+ */
+public class Waiter implements Observer {
+
+    @Override
+    public void update(java.util.Observable o, Object arg) {
+
+    }
+}
