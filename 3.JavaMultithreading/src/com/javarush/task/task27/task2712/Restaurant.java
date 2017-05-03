408f83d3327a87cde85a4feb7568697256109a75
Dmitriy Rokachov <rokachov@gmail.com>
2017-05-03 15:37:34 +0300

    2712.2 +

---
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2712/Restaurant.java
@@ -0,0 +1,10 @@
+package com.javarush.task.task27.task2712;
+
+/**
+ * Created by Rumata on 03.05.2017.
+ */
+public class Restaurant {
+    public static void main(String[] args) {
+        new Tablet(5).createOrder();
+    }
+}
