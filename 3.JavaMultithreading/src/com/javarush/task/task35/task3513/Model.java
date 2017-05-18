b9912b5ca44af587065428b2bf0191c3814dcc85
Dmitriy Rokachov <rokachov@gmail.com>
2017-05-18 17:51:23 +0300

    "2048 game"  3513.4 +

--- 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Model.java
+++ 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Model.java
@@ -1,7 +1,44 @@
 package com.javarush.task.task35.task3513;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Rumata on 18.05.2017.
  */
 public class Model {
+    private static final int FIELD_WIDTH = 4;
+    private Tile[][] gameTiles;
+
+    public Model() {
+        resetGameTiles();
+    }
+
+    List<Tile> getEmptyTiles() {
+        List<Tile> result = new ArrayList<>();
+        for (int i = 0; i < FIELD_WIDTH; i++) {
+            for (int j = 0; j < FIELD_WIDTH; j++) {
+                if (gameTiles[i][j].value == 0) result.add(gameTiles[i][j]);
+            }
+        }
+        return result;
+    }
+
+    void addTile() {
+        List<Tile> list = getEmptyTiles();
+        list.get((int)(list.size()*Math.random())).setValue(Math.random() < 0.9 ? 2 : 4);
+    }
+
+    void resetGameTiles() {
+        this.gameTiles = new Tile[FIELD_WIDTH][FIELD_WIDTH];
+        for (int i = 0; i < FIELD_WIDTH; i++) {
+            for (int j = 0; j < FIELD_WIDTH; j++) {
+                this.gameTiles[i][j] = new Tile();
+            }
+        }
+        addTile();
+        addTile();
+    }
+
+
 }
---
+++ 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Tile.java
@@ -0,0 +1,76 @@
+package com.javarush.task.task35.task3513;
+
+import java.awt.*;
+
+/**
+ * Created by Rumata on 18.05.2017.
+ */
+public class Tile {
+    int value;
+
+    public Tile(int value) {
+        this.value = value;
+    }
+
+    public Tile() {
+        this.value = 0;
+    }
+
+    public boolean isEmpty() {
+        return value == 0;
+    }
+
+    public Color getFontColor() {
+        return new Color(value < 16 ? 0x776e65 : 0xf9f6f2);
+    }
+
+    public Color getTileColor() {
+        Color color;
+        switch (value) {
+            case 0:
+                color = new Color(0xcdc1b4);
+                break;
+            case 2:
+                color = new Color(0xeee4da);
+                break;
+            case 4:
+                color = new Color(0xede0c8);
+                break;
+            case 8:
+                color = new Color(0xf2b179);
+                break;
+            case 16:
+                color = new Color(0xf59563);
+                break;
+            case 32:
+                color = new Color(0xf67c5f);
+                break;
+            case 64:
+                color = new Color(0xf65e3b);
+                break;
+            case 128:
+                color = new Color(0xedcf72);
+                break;
+            case 256:
+                color = new Color(0xedcc61);
+                break;
+            case 512:
+                color = new Color(0xedc850);
+                break;
+            case 1024:
+                color = new Color(0xedc53f);
+                break;
+            case 2048:
+                color = new Color(0xedc22e);
+                break;
+            default:
+                color = new Color(0xff0000);
+                break;
+        }
+        return color;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+}
