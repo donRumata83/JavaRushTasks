deb541d8772df1c6aeebf127296864a07da919ea
Dmitriy Rokachov <rokachov@gmail.com>
2017-05-18 14:30:05 +0300

    "2048 game"  3513.1 +

---
+++ 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Main.java
@@ -0,0 +1,10 @@
+package com.javarush.task.task35.task3513;
+
+/**
+ * Created by Rumata on 18.05.2017.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ 3.JavaMultithreading/src/com/javarush/task/task35/task3513/View.java
@@ -0,0 +1,7 @@
+package com.javarush.task.task35.task3513;
+
+/**
+ * Created by Rumata on 18.05.2017.
+ */
+public class View {
+}
