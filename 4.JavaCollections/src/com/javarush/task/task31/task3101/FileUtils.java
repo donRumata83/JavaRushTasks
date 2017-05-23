8449b40a30bb95e57c94eda610521a99f2ea4e98
Dmitriy Rokachov <rokachov@gmail.com>
2017-05-23 18:02:24 +0300

    3101

--- 4.JavaCollections/src/com/javarush/task/task31/task3101/FileUtils.java
+++ 4.JavaCollections/src/com/javarush/task/task31/task3101/FileUtils.java
@@ -1,11 +1,6 @@
 package com.javarush.task.task31.task3101;
 
 import java.io.File;
-import java.io.IOException;
-import java.nio.file.FileVisitResult;
-import java.nio.file.Path;
-import java.nio.file.SimpleFileVisitor;
-import java.nio.file.attribute.BasicFileAttributes;
 
 public class FileUtils {
 
@@ -15,16 +10,5 @@ public class FileUtils {
 
     public static void renameFile(File source, File destination) {
         if (!source.renameTo(destination)) System.out.println("Can not rename file with name " + source.getName());
-    }
-}
-    class MyFileVisitor extends SimpleFileVisitor
-{
-
-    public FileVisitResult visitFile(Path path, BasicFileAttributes attrs) throws IOException {
-        File file = path.toFile();
-        if (file.length() <= 50) {Solution.listOfFiles.add(path);}
-        else FileUtils.deleteFile(file);
-
-        return FileVisitResult.CONTINUE;
     }
 }
--- 4.JavaCollections/src/com/javarush/task/task31/task3101/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task31/task3101/Solution.java
@@ -1,36 +1,81 @@
 package com.javarush.task.task31.task3101;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.*;
 
+import java.io.File;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
-import java.util.List;
+import java.util.PriorityQueue;
 
 /*
 Проход по дереву файлов
 */
 public class Solution {
-    public static List<Path> listOfFiles;
-
-
     public static void main(String[] args) {
-        try {
-            Files.walkFileTree(Paths.get(args[0]), new MyFileVisitor());
-        } catch (IOException e) {e.printStackTrace();}
-        Collections.sort(listOfFiles, new Comparator<Path>() {
+        File pathToSearch = new File(args[0]);
+        File targetFile = new File(args[1]);
+        PriorityQueue<File> queue = new PriorityQueue<>();
+        ArrayList<File> fileList = new ArrayList<>();
+        queue.add(pathToSearch);
+        Path path;
+        File tmp;
+        while (!queue.isEmpty()) {
+            tmp = queue.poll();
+            path = tmp.toPath();
+            if (!tmp.equals(targetFile)) {
+                if (Files.isRegularFile(path)) {
+                    if (tmp.length() <= 50) {
+                        fileList.add(tmp);
+                    } else FileUtils.deleteFile(tmp);
+                }
+
+                if (Files.isDirectory(path)) {
+                    try (DirectoryStream<Path> directoryStream = Files.newDirectoryStream(path)) {
+                        for (Path file : directoryStream) {
+                            queue.add(file.toFile());
+                        }
+                    } catch (IOException e) {
+                    }
+
+                }
+            }
+        }
+
+        Collections.sort(fileList, new Comparator<File>() {
             @Override
-            public int compare(Path o1, Path o2) {
-                return o1.getFileName().compareTo(o2.getFileName());
+            public int compare(File o1, File o2) {
+                return o1.getName().compareTo(o2.getName());
             }
         });
-        Path pathToFile = Paths.get(args[1]);
 
-        try {
-        Files.createFile(pathToFile);
-        FileUtils.renameFile();
-        } catch (IOException e) {}
+        String targetDirectory = targetFile.getAbsolutePath().substring(0, targetFile.getAbsolutePath().lastIndexOf(File.separator) + 1);
+        File newTargetFile = new File(targetDirectory + "allFilesContent.txt");
+        FileUtils.renameFile(targetFile, newTargetFile);
+
+        char[] buffer = new char[1000];
+        int count = 0;
+        try (FileWriter fileWriter = new FileWriter(newTargetFile)) {
+            for (File file : fileList) {
+                try (FileReader fileReader = new FileReader(file)) {
+                    while (fileReader.ready()) {
+                        count = fileReader.read(buffer);
+                        fileWriter.write(buffer, 0, count);
+                    }
+                    fileWriter.write("\n");
+                } catch (IOException e) {
+                }
+            }
+        } catch (IOException e) {
+        }
+
+
     }
 
     public static void deleteFile(File file) {
